b87aa31949a855be2233b4606b2a58dd0f7958d8
aishabutt1011 <aishabutt1011@gmail.com>
2025-03-17 22:27:55 +0500

    Update comments.py

--- comments.py
+++ comments.py
@@ -1,3 +1,8 @@
 # A comment begins with a hash character(#) which is not a part of the string literal and ends at the end of the physical line. 
 # All characters after the # character up to the end of the line are part of the comment.
 # The Python interpreter ignores them.
+# we use triple single or double qoute to wite multi line comment
+"""
+my name is aisha
+i am 20 years oold
+"""
